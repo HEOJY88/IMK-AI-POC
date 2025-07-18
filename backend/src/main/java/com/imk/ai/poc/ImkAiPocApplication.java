7efb1f4dd9c6efc1ecf72baa5aa475a08054162f
hjy <junyeong.heo@imarketkorea.com>
2025-07-18 14:24:46 +0900

    프로젝트 디렉토리 최초 구성

---
+++ backend/src/main/java/com/imk/ai/poc/ImkAiPocApplication.java
@@ -0,0 +1,13 @@
+package com.imk.ai.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImkAiPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImkAiPocApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/imk/ai/poc/ImkAiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imk.ai.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImkAiPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
